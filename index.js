51ca4a63c1ef030d8e30214a2d19e0ce17c990b1
AxelCortes162 <axelcorjmaxd@gmail.com>
2025-12-11 14:35:16 -0600

    nav

---
+++ index.js
@@ -0,0 +1,28 @@
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    const navLinks = document.querySelectorAll('nav a');
+    const overlay = document.querySelector('.mobile-overlay');
+
+    menuToggle.addEventListener('click', () => {
+        const isOpen = nav.classList.toggle('show');
+        overlay.classList.toggle('show');
+
+        // Cambiar ícono
+        menuToggle.textContent = isOpen ? '✕' : '☰';
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('show');
+            overlay.classList.remove('show');
+            menuToggle.textContent = '☰'; // volver a hamburguesa
+        });
+    });
+
+    overlay.addEventListener('click', () => {
+        nav.classList.remove('show');
+        overlay.classList.remove('show');
+        menuToggle.textContent = '☰'; // volver a hamburguesa
+    });
+
